1dca9996b3f2d9ab7da60911da577b4112e2ef05
Anuj Hoslok <anujhoslokdev@gmail.com>
2022-11-10 06:13:27 +0530

    added a function that loops through the library and displays the book present in it

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 const submitBtn = document.getElementById('submitBtn')
+let booksInLibrary = document.getElementById('booksInLibrary')
 let bookName;
 let pages;
 let readStatus;
 let myLibrary =[]
 
-
-
+//actions which take place upon hitting the submit button
 submitBtn.onclick = function addBookToLibrary(){
     bookName = document.getElementById('bookName').value;
     myLibrary.push(bookName)
+    temp();
     document.getElementById('bookName').value = ""
     console.log(myLibrary)
 }
 
+let holdsBook="";
+function temp()
+{
+    booksInLibrary.textContent=myLibrary;
+}
 
 
 
-
-
-
-
-
-document.getElementById('resetBtn').onclick = function(){
+document.getElementById('resetBtn').onclick = function clearLibrary(){
     myLibrary=[]
+    booksInLibrary.textContent="Books present"
     console.log(myLibrary)
 }
